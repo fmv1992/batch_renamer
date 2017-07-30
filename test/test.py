c231975bf77ee135adb112d69d9a886a515c9e30
Felipe M. Vieira <fmv1992@gmail.com>
2017-07-30 12:09:54 -0300

    improved tests primitives

--- test/test.py
+++ test/test.py
@@ -1,66 +1,102 @@
-import random
-import math
 import string
+import random
 import os
-import tempfile
-import inspect
-import datetime
 import unittest
+import tempfile
+import datetime
+import math
+
 from batch_renamer.batch_renamer import primitive_name, generate_folder_structure, add_trailing_number, prefix_iso_mod_date  # noqa
 
-# pylama:ignore=D100,D101,D102
 
+# pylama:ignore=D100,D101,D102,D103,E731
 PATH_RESERVED_SYMBOLS = set(('\\', '/'))
 ALLOWED_SYMBOLS = set(('_', '.'))
 NON_ALLOWED_SYMBOLS = (set(string.punctuation)
                        - (ALLOWED_SYMBOLS | PATH_RESERVED_SYMBOLS))
-
 N = 500  # Number of strings in each folder.
 
 
-def create_string_from_set(set_of_chars,
-                           n=100,
-                           min_len=10,
-                           max_len=100):
-    """Return a map of n strings between min and max lenght."""
-    lenght_of_strings = (random.randint(min_len, max_len) for x in range(n))
-    strings = map(
-        lambda len_of_str: ''.join(
-            random.choice(tuple(set_of_chars)) for x in range(len_of_str)),
-        lenght_of_strings)
-    return strings
+def create_random_string(allowed_chars=string.ascii_lowercase,
+                         min_len=10,
+                         max_len=100):
+    return ''.join(random.choice(tuple(allowed_chars)) for x in
+                   range(random.randint(min_len, max_len)))
 
 
-def populate_folder(folder_path,
-                    symbol_set,
-                    recursive=False,
-                    rec_branching_probability=0.5,
-                    rec_initial_child_folders=5):
-    """Helper function to populate folder with files.
+def create_file_extension(extension=None):
+    if extension is None:
+        all_extensions = ('txt', 'mp3', 'py', 'tar.xz', '.tar', )
+        extension = random.choice(all_extensions)
+    return extension
 
-    Helper function to populate folder with files drawing names from symbol
-    set.
 
-    Arguments:
-        folder_path (str): folder path to populate.
-        symbol_set (set): set of chars allowed to compose filename.
+def create_filename(
+        root_dir,
+        include_extension=False,
+        kwargs_create_file_extension=dict(),
+        kwargs_create_random_string=dict()):
+    if include_extension:
+        extension = create_file_extension(**kwargs_create_file_extension)
+    else:
+        extension = ''
+    return os.path.join(
+        root_dir,
+        create_random_string(**kwargs_create_random_string)
+        + extension)
 
-    """
-    # # Create folders recursively.
-    # if recursive:
-    #     for _, dir_name in enumerate(create_string_from_set(symbol_set)):
-    #         file_path = os.path.join(folder_path, file_name)
-    #         while os.path.exists(dir_name):
-    #             dir_name = dir_name + random.choice(tuple(symbol_set))
-    #         os.mkdir(file_path)
 
-    # Populate folders recursively.
-    for file_name in create_string_from_set(symbol_set):
-        file_path = os.path.join(folder_path, file_name)
-        while os.path.exists(file_path):
-            file_path = file_path + random.choice(tuple(symbol_set))
-        os.mknod(file_path)
+def create_dirname(root_dir, kwargs_create_random_string=dict()):
+    return os.path.join(root_dir,
+                        create_random_string(**kwargs_create_random_string))
+
+
+def populate_directory_with_files(
+        root_dir,
+        include_extension=False,
+        n_min_files=5,
+        n_max_files=10,
+        kwargs_create_file_extension=dict(),
+        kwargs_create_random_string=dict()):
+
+    call_f = lambda: create_filename(
+        root_dir,
+        include_extension=include_extension,
+        kwargs_create_file_extension=kwargs_create_file_extension,
+        kwargs_create_random_string=kwargs_create_random_string)
+
+    for fname in (call_f() for x in range(
+            random.randint(n_min_files, n_max_files))):
+        # print(fname)
+        os.mknod(fname)
+
     return None
+
+
+def populate_directory_with_dirs(
+        root_dir,
+        n_min_dirs=5,
+        n_max_dirs=10,
+        kwargs_create_file_extension=dict(),
+        kwargs_create_random_string=dict()):
+
+    call_f = lambda: create_dirname(root_dir, **kwargs_create_random_string)
+
+    for dirname in (call_f() for x in range(
+            random.randint(n_min_dirs, n_max_dirs))):
+        # print(dirname)
+        os.mkdir(dirname)
+
+    return None
+
+
+def recursive_populate_directory_with_dirs():
+    pass
+
+
+def recursive_populate_directory_with_files():
+    # Ensure in every node.
+    pass
 
 
 class TestBatchRenamer(unittest.TestCase):
@@ -83,32 +119,37 @@ class TestBatchRenamer(unittest.TestCase):
         # TODO: why now?
         self.now = datetime.datetime.now()
         # Initialize excl_symbols_folder.
-        populate_folder(self.non_compliant_folder, NON_ALLOWED_SYMBOLS)
+        populate_directory_with_files(
+            self.non_compliant_folder,
+            kwargs_create_random_string={'allowed_chars': NON_ALLOWED_SYMBOLS})
         # Initialize compliant_names_folder.
-        populate_folder(self.compliant_folder,
-                        string.ascii_lowercase)
+        populate_directory_with_files(
+            self.compliant_folder,
+            kwargs_create_random_string={
+                'allowed_chars': string.ascii_lowercase})
 
     def tearDown(self):
         self._program_folder.cleanup()
 
     def test_primitive_name(self):
         # Testing with allowed strings.
-        for s in create_string_from_set(string.ascii_lowercase,
-                                        n=N):
+        for _, s in zip(range(N), create_random_string(ALLOWED_SYMBOLS)):
             with self.subTest(s=s):
                 self.assertEqual(s, primitive_name(s))
         # Testing with non allowed strings.
-        for s in create_string_from_set(NON_ALLOWED_SYMBOLS,
-                                        n=N,
-                                        min_len=2):
+        for _, s in zip(range(N),
+                        create_random_string(
+                            NON_ALLOWED_SYMBOLS,
+                            min_len=2)):
             with self.subTest(s=s):
                 self.assertEqual('_', primitive_name(s))
         # Testing with sequence of allowed strings with some underscore
         # sequence:
-        for s in create_string_from_set(string.ascii_lowercase,
-                                        n=N,
-                                        min_len=2,
-                                        max_len=100):
+
+        call_f = lambda: create_random_string(string.ascii_lowercase,
+                                              min_len=2, max_len=100)
+
+        for s in (call_f() for x in range(N)):
             cutting_point = random.randint(1, len(s) - 1)
             s = s[:cutting_point] + '__' + s[cutting_point:]
             with self.subTest(s=s):
@@ -120,10 +161,9 @@ class TestBatchRenamer(unittest.TestCase):
         iterable_sizes = (10 ** x for x in range(1, 5))
         for size in iterable_sizes:
             for s in add_trailing_number(
-                    create_string_from_set(set('a'),
-                                           n=size,
-                                           min_len=1,
-                                           max_len=1),
+                 create_random_string(set('a'),
+                                      min_len=1,
+                                      max_len=1),
                     n=size):
                 self.assertEqual(len(s), round(math.log(size, 10) + 2))
 
@@ -138,7 +178,7 @@ class TestBatchRenamer(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromTestCase(BatchRenamerTest)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestBatchRenamer)
     unittest.TextTestRunner(verbosity=2).run(suite)
     # print(create_string_from_set('abcde', 100))
     # print(add_trailing_number(list('abcdefghijklmonopqrst')))
